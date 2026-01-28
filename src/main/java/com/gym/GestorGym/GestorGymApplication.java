c9ce25637e3cbd471ca06843e8104796859d022b
Kevin <kfornier@gmail.com>
2026-01-28 09:48:45 -0300

    edit

--- src/main/java/com/gym/GestorGym/GestorGymApplication.java
+++ src/main/java/com/gym/GestorGym/GestorGymApplication.java
@@ -2,8 +2,9 @@ package com.gym.GestorGym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GestorGymApplication {
 
 	public static void main(String[] args) {
