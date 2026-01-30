9c3118ed95742ccdd719ee7dbe197bebdbd640d8
Kevin <kfornier@gmail.com>
2026-01-30 09:57:53 -0300

    db connection

--- src/main/java/com/gym/GestorGym/GestorGymApplication.java
+++ src/main/java/com/gym/GestorGym/GestorGymApplication.java
@@ -2,9 +2,8 @@ package com.gym.GestorGym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class GestorGymApplication {
 
 	public static void main(String[] args) {
