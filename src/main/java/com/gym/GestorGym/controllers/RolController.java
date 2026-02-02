40cf0634fd4756f6d75f793618567951760c7a0f
Kevin <kfornier@gmail.com>
2026-02-02 11:26:57 -0300

    estructura rol

--- src/main/java/com/gym/GestorGym/controllers/RolController.java
+++ src/main/java/com/gym/GestorGym/controllers/RolController.java
@@ -1,10 +1,27 @@
 package com.gym.GestorGym.controllers;
 
+import com.gym.GestorGym.dto.RolDTO;
+import com.gym.GestorGym.repository.RolRepository;
+import com.gym.GestorGym.service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/rol")
 public class RolController {
+    @Autowired
+    private RolRepository rolRepository;
+    @Autowired
+    private RolService rolService;
+
+    @PostMapping("/crear")
+    public String crear(@RequestBody RolDTO rolDTO) {
+        rolService.crear(rolDTO);
+
+        return "Rol creado";
+    }
 
 }
