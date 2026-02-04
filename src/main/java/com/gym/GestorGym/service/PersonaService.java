2d7fad99eb9bbf90574efea22e0bd34f1729b349
CamiloDce <camilod0310@gmail.com>
2026-02-04 12:08:37 -0300

    estable

--- src/main/java/com/gym/GestorGym/service/PersonaService.java
+++ src/main/java/com/gym/GestorGym/service/PersonaService.java
@@ -7,7 +7,6 @@ import com.gym.GestorGym.mapper.Mapper;
 import com.gym.GestorGym.models.Persona;
 import com.gym.GestorGym.repository.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
